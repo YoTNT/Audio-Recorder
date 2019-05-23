c721b8128e1e79ee7ccf6e3868ad2a83a40ccec5
xiaochuanfang <xiaochuan1991@hotmail.com>
2019-05-23 16:10:49 -0400

    New main

--- app/src/main/java/com/codepath/audiorecorder/MainActivity.java
+++ app/src/main/java/com/codepath/audiorecorder/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.fragment_posts);
+        //setContentView(R.layout.fragment_posts);
 
     }
 }
